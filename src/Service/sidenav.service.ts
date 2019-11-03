8ee19723d765eaf878a60c4b0af13c2f26ef582c
unknown <mladenko96@gmail.com>
2019-11-03 11:52:38 +0100

    novo

---
+++ src/Service/sidenav.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+@Injectable()
+export class SideNavService {
+ public sideNavToggleSubject: BehaviorSubject<any> = new BehaviorSubject(null);
+
+  constructor() { } 
+ 
+
+  public toggle() {
+    return this.sideNavToggleSubject.next(null);
+  } 
+}
