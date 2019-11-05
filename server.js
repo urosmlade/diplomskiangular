0962e93a0d9e32844c09b7ed0faaac1f02c6d1cc
unknown <mladenko96@gmail.com>
2019-11-05 10:06:49 +0100

    make it better

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = requere('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Flashcards'));
 
 app.listen(process.env.PORT || 8080);
 
 //PathLocationStrategy
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Flashcards/index.html'));
 })
 
 console.log('Console listening');
