d609be66a462d04f69579b81c4c9171806f477a7
unknown <mladenko96@gmail.com>
2019-11-05 10:25:34 +0100

    make it better

--- server.js
+++ server.js
@@ -14,3 +14,8 @@ app.get('/*', function(req,res){
 })
 
 console.log('Console listening');
+
+var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+  });
